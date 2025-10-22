b6693148bd4fc67c14a12d17c8020ff0d85bef9e
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-10-22 20:21:03 -0300

    Refactor test descriptions for clarity and consistency

--- src/tests/api.test.ts
+++ src/tests/api.test.ts
@@ -25,47 +25,47 @@ describe("Mock function tests", () => {
     ],
   };
 
-  it("usa mockReturnValue()", async () => {
+  it("mockReturnValue()", async () => {
     (api.get as jest.Mock).mockReturnValue({ data: mockCharacters });
 
     const data = await getCharacteres();
     expect(data.results.length).toBe(2);
   });
 
-  it("usa mockReturnValueOnce()", async () => {
+  it("mockReturnValueOnce()", async () => {
     (api.get as jest.Mock).mockReturnValueOnce({ data: mockCharacters });
 
     const data = await getCharacteres();
     expect(data.info.count).toBe(2);
   });
 
-  it("usa mockResolvedValue()", async () => {
+  it("mockResolvedValue()", async () => {
     (api.get as jest.Mock).mockResolvedValue({ data: mockCharacters });
 
     const data = await getCharacteres();
     expect(data.results[0].name).toBe("Rick Sanchez");
   });
 
-  it("usa mockResolvedValueOnce()", async () => {
+  it("umockResolvedValueOnce()", async () => {
     (api.get as jest.Mock).mockResolvedValueOnce({ data: mockCharacters });
 
     const data = await getCharacteres();
     expect(data.results[1].name).toBe("Morty Smith");
   });
 
-  it("usa mockRejectedValue()", async () => {
+  it("mockRejectedValue()", async () => {
     (api.get as jest.Mock).mockRejectedValue(new Error("Erro genérico"));
 
     await expect(getCharacteres()).rejects.toThrow("Erro genérico");
   });
 
-  it("usa mockRejectedValueOnce()", async () => {
+  it("mockRejectedValueOnce()", async () => {
     (api.get as jest.Mock).mockRejectedValueOnce(new Error("Falha temporária"));
 
     await expect(getCharacteres()).rejects.toThrow("Falha temporária");
   });
 
-  it("usa mockImplementation()", async () => {
+  it("mockImplementation()", async () => {
     (api.get as jest.Mock).mockImplementation(async (url: string) => {
       return { data: mockCharacters };
     });
@@ -74,7 +74,7 @@ describe("Mock function tests", () => {
     expect(data.info.pages).toBe(1);
   });
 
-  it("usa mockImplementationOnce()", async () => {
+  it("mockImplementationOnce()", async () => {
     (api.get as jest.Mock).mockImplementationOnce(async () => {
       return { data: mockCharacters };
     });
